1f3585e4c6b26726eb0a12c4ae724a009a0b6abd
aq032 <anthonyq032@gmail.com>
2022-06-27 15:31:42 -0700

    Cards now update with random recipe info

--- assets/js/index.js
+++ assets/js/index.js
@@ -24,56 +24,6 @@ let theme = 'dark';
 
 const nav = document.getElementById('nav')
 
-//Spoonacular API
-//For every request to Spoonacular, the first query parameter has to be the API key
-//Example url: 'https://api.spoonacular.com/recipes/complexSearch?apiKey=da1414212d52482cbe9aaf669cae5da3'
-
-//Use the following query parameters
-//sort=random - makes the foods returned random
-//instructionsRequired=true - recipes must have instructions
-//addRecipeInformation=true - if set to true, you get more information about the recipe returned
-//maxReadyTime=30 - the maximum time in minutes it should take to prepare and cook a recipe
-//type= - 
-//excludeIngredients - A comma-separated list of ingredients that the recipes must not contain
-
-//id=12312 - number is an example, but gets specific information from
-
-//NEED TO FIND INGREDIENTS
-
-function getSpoonApi() {
-    let requestUrl = "https://api.spoonacular.com/recipes/complexSearch?apiKey=da1414212d52482cbe9aaf669cae5da3&sort=random&number=3&instructionsRequired=true&addRecipeInformation=true&maxReadyTime=60"
-
-    fetch(requestUrl)
-        .then(function(response) {
-            return response.json();
-        })
-
-        .then(function(data) {
-            console.log(data.results);
-            for (let i = 0; i < data.results.length; i++) {
-                let recipeSteps = data.results[i].analyzedInstructions[0].steps;
-
-                console.log("This is dish #" + i);
-                console.log("-----------------------");
-                console.log(data.results[i].title);
-                console.log(data.results[i].readyInMinutes);
-                console.log(data.results[i].servings);
-                console.log(data.results[i].sourceUrl);
-                console.log(data.results[i].image);
-                console.log(recipeSteps);
-
-                //This is to find the instructions
-                for (let j = 0; j < recipeSteps.length; j++) {
-                    console.log(recipeSteps[j].number);
-                    console.log(recipeSteps[j].step);
-                }
-
-                console.log("");
-
-            }
-        })
-
-}
 // Check if there is something in localstorage 
 function restoreStorage() {
   let themes = localStorage.getItem('theme');
@@ -160,3 +110,107 @@ function toggleTheme(e) {
 
 // Event Listeners 
 toggleThemeBtn.on('click', toggleTheme)
+
+
+
+//Spoonacular API
+//For every request to Spoonacular, the first query parameter has to be the API key
+//Example url: 'https://api.spoonacular.com/recipes/complexSearch?apiKey=da1414212d52482cbe9aaf669cae5da3'
+
+//Use the following query parameters
+//sort=random - makes the foods returned random
+//instructionsRequired=true - recipes must have instructions
+//addRecipeInformation=true - if set to true, you get more information about the recipe returned
+//maxReadyTime=30 - the maximum time in minutes it should take to prepare and cook a recipe
+//type= - 
+//excludeIngredients - A comma-separated list of ingredients that the recipes must not contain
+
+//id=12312 - number is an example, but gets specific information from
+
+const testButton = document.querySelector("#test-button");
+testButton.addEventListener("click", getSpoonApi);
+
+function getSpoonApi() {
+    let requestUrl = "https://api.spoonacular.com/recipes/complexSearch?apiKey=da1414212d52482cbe9aaf669cae5da3&sort=random&number=3&instructionsRequired=true&addRecipeInformation=true&maxReadyTime=60&fillIngredients=true&type=maincourse"
+
+    fetch(requestUrl)
+        .then(function(response) {
+            return response.json();
+        })
+
+        .then(function(data) {
+            console.log(data.results);
+            dataResults = data.results;
+            //return dataResults;
+
+            for (let i = 0; i < data.results.length; i++) {
+
+                //console log section
+                let recipeSteps = data.results[i].analyzedInstructions[0].steps;
+
+                console.log("This is dish #" + i);
+                console.log("-----------------------");
+
+                let cardTitle = data.results[i].title;
+                console.log(cardTitle);
+
+                let prepTime = data.results[i].readyInMinutes;
+                console.log(prepTime + " minutes");
+
+                let cardSummary = data.results[i].summary;
+                console.log(cardSummary);
+
+                console.log(data.results[i].servings + " servings");
+                console.log(data.results[i].sourceUrl);
+
+                let cardImage = data.results[i].image;
+                console.log(cardImage);
+
+                console.log(recipeSteps);
+
+            //Card section
+
+                //Changes card images
+                let currentCardImg = document.getElementsByClassName("food-card-img");
+                currentCardImg[i].src = cardImage;
+
+                //Changes card title
+                let currentCardTitle = document.getElementsByClassName("food-card-title");
+                currentCardTitle[i].textContent = "";
+                currentCardTitle[i].textContent = cardTitle;
+
+                //Change prep time
+                let currentPrepTime = document.getElementsByClassName("prep-time");
+                currentPrepTime[i].textContent = prepTime + " minutes";
+
+                //Change inside card summary
+                let currentCardSummary = document.getElementsByClassName("recipe-summary");
+                currentCardSummary[i].textContent = "";
+                currentCardSummary[i].textContent = cardSummary;
+
+                //This is to find the ingredients
+                let ingredients = data.results[i].extendedIngredients;
+                console.log("The ingredients for this dish are: ")
+
+                for (let a = 0; a < ingredients.length; a++) {
+                    console.log(ingredients[a].original);
+                }
+
+                console.log("The steps for this dish are: ")
+                //This is to find the instructions
+                for (let j = 0; j < recipeSteps.length; j++) {
+                    console.log(recipeSteps[j].number);
+                    console.log(recipeSteps[j].step);
+                }
+
+                console.log("");
+
+            }
+        })
+    return
+
+}
+
+// let dataResults = await getSpoonApi();
+
+// console.log(dataResults.length);
